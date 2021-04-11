aada8848590a9e77fc77f8f67cff8385f0fd71c8
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-04-11 21:05:23 +0200

    When someone chooses the wrong flag, the alert message display the name of the wrong flag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -61,7 +61,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, that's the flag of \(countries[number])"
             score -= 1
         }
         showingScore = true
